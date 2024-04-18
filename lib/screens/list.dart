c07018c31326923f32763378e5ee1e7d13a896b6
Norbert Szekeres <norbert.szekeres@gmail.com>
2024-04-18 16:14:24 +0200

    add some data, model and list screen

---
+++ lib/screens/list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+      ),
+      body: ListView(
+        children: [
+          for (final item in groceryItems)
+            ListTile(
+              leading: Checkbox(
+                value: false,
+                onChanged: (value) {},
+                checkColor: Colors.white,
+                fillColor: MaterialStateProperty.all(item.category.color),
+                side: BorderSide(
+                  color: item.category.color,
+                  width: 3,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+              title: Text(item.name),
+              trailing: Text(item.quantity.toString()),
+            ),
+        ],
+      ),
+    );
+  }
+}
